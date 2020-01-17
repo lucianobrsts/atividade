26fe517ec98b98bb72015c84db5790e9e9d61435
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-01-17 15:03:33 -0300

    Commmit Inicial - Projeto corrigido.

---
+++ atividade/src/br/com/atividade/paginaprincipal/PaginaPrincipalBean.java
@@ -0,0 +1,17 @@
+package br.com.atividade.paginaprincipal;
+
+import java.io.Serializable;
+
+/** 
+ * Prova elaborada por Daniel Oliveira, para avaliação dos candidatos ao projeto Vitae.
+ * Fortaleza, 07/01/2020. 
+*/
+public class PaginaPrincipalBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6791807627637800136L;
+	
+
+}

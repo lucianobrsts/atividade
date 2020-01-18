1b5a483e7ffc1c831b19023e8762de716d6975ec
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-01-18 13:11:28 -0300

    Mapear a classe paciente com anotações JPA

---
+++ atividade/src/br/com/atividade/dao/PacienteDAO.java
@@ -0,0 +1,11 @@
+package br.com.atividade.dao;
+
+import br.com.atividade.domain.Paciente;
+
+public class PacienteDAO {
+
+	public void salvar(Paciente paciente) {
+
+	}
+
+}
--- atividade/src/br/com/atividade/domain/Paciente.java
+++ atividade/src/br/com/atividade/domain/Paciente.java
@@ -1,9 +1,25 @@
 package br.com.atividade.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "paciente")
 public class Paciente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Long id;
+
+	@Column(name = "nome")
 	private String nome;
+
+	@Column(name = "idade")
 	private Integer idade;
 
 	public Long getId() {

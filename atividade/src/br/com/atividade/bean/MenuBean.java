5f188dfd7e0aea7fb6d190942bf10516dfcd6465
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-01-17 22:32:39 -0300

    Criar Cadastro paciente - navegação e view

---
+++ atividade/src/br/com/atividade/bean/MenuBean.java
@@ -0,0 +1,16 @@
+package br.com.atividade.bean;
+
+import java.io.Serializable;
+
+public class MenuBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String pacienteCadastro() {
+		return "pacienteCadastro";
+	}
+
+}

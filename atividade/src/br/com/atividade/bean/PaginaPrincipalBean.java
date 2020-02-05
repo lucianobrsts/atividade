aed7da2bf2c53c9f7e372556e23df1afb23ca243
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-02-05 16:08:22 -0300

    Modificada regras de navegação no faces-config

--- atividade/src/br/com/atividade/bean/PaginaPrincipalBean.java
+++ atividade/src/br/com/atividade/bean/PaginaPrincipalBean.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.annotation.ManagedBean;
 
 /**
- * Prova elaborada por Daniel Oliveira, para avaliação dos candidatos ao projeto
+ * Prova elaborada por Daniel Oliveira, para avaliaï¿½ï¿½o dos candidatos ao projeto
  * Vitae. Fortaleza, 07/01/2020.
  */
 @ManagedBean
@@ -17,7 +17,7 @@ public class PaginaPrincipalBean implements Serializable {
 	private static final long serialVersionUID = -6791807627637800136L;
 
 	public String paginaMenu() {
-		return "pricipalMenu";
+		return "principalMenu";
 	}
 
 }

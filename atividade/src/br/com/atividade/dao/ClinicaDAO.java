f9e89a217c768a144e88e125828116904a5d9660
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-02-08 23:02:46 -0300

    Criada Interface ClinicaDAO e classe ClinicaDAOImpl

---
+++ atividade/src/br/com/atividade/dao/ClinicaDAO.java
@@ -0,0 +1,7 @@
+package br.com.atividade.dao;
+
+import br.com.atividade.domain.Clinica;
+
+public interface ClinicaDAO extends AbstractDAO<Clinica, Long> {
+
+}
---
+++ atividade/src/br/com/atividade/dao/ClinicaDAOImpl.java
@@ -0,0 +1,12 @@
+package br.com.atividade.dao;
+
+import br.com.atividade.domain.Clinica;
+
+public class ClinicaDAOImpl extends AbstractDAOImpl<Clinica, Long> implements ClinicaDAO {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
